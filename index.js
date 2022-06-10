3ad8641edc63567ed814b92d5de4669483fc9ef7
pushpitanandy <nandypushpita@gmail.com>
2022-06-11 00:22:01 +0600

    changes while loading single project done

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ async function run() {
         });
 
         app.get('/projects/:id', async (req, res) => {
-            const id = req.query.id;
+            const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const project = await projectCollection.findOne(query);
             res.send(project);
